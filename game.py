c3422fd5d1158d42682d87d2b63f47813da1554d
sshtayn <ss13523@stern.nyu.edu>
2021-05-25 21:10:10 -0400

    Inc if else Function

--- game.py
+++ game.py
@@ -1,7 +1,24 @@
 # game.py
 
+
+
 print("Rock, Paper, Scissors, Shoot!")
 
 user_choice=input("Please choose one of 'rock', 'paper', 'scissors': ")
 
 print("USER CHOICE:",user_choice)
+
+
+if (user_choice == "rock")  or (user_choice == "paper") or (user_choice == "scissors"):
+    print("Valid. KEEP GOING")
+else:
+    print("OOPS, invalid input. Please Try again.")
+    exit()
+
+import random
+valid_options = ["rock", "paper", "scissors"]
+computer_choice = random.choice(valid_options)
+print("COMPUTER CHOICE: ", computer_choice)
+
+
+print("THIS IS THE END OF OUR GAME. PLEASE PLAY AGAIN")
